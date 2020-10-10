0e02e90dd363567410eda7bc2846d8a7e4527a1d
TearingVoo <alexlisdream@gmail.com>
2020-10-10 17:35:10 +0300

    commit

--- Menu.js
+++ Menu.js
@@ -1,1 +1,4 @@
-let aboutMe = document.createElement('div');
+function showDisplay() {
+    let  aboutMe = document.querySelectorAll('.icon').forEach(n => n.classList.remove('hidden'))
+};
+setTimeout(showDisplay, 7500);
--- loader.js
+++ loader.js
@@ -1,25 +1,23 @@
-    let loader = document.getElementById("loader");
-    let loaderDel= document.querySelector(".loader");
-    let loaderText = 'Loading...';
-    let loaderIndex = -1;
-    let timer;
-    function start() {
-        timer = window.setInterval(function () {
-            if (++loaderIndex === loaderText.length)
-                clearInterval(timer);
-            else
-                loader.innerHTML += loaderText[loaderIndex] + '';
-        }, 100);
+let loader = document.getElementById("loader");
+let loaderDel = document.querySelector(".loader");
+let loaderText = 'Loading...';
+let loaderIndex = -1;
+function start() {
+   let timer = window.setInterval(function () {
+        if (++loaderIndex === loaderText.length)
+            clearInterval(timer);
+        else
+            loader.innerHTML += loaderText[loaderIndex] + '';
+    }, 100);
+}
+count = 0;
+intervalId = setInterval(function () {
+    count++;
+    if (count === 5) {
+        clearInterval(intervalId);
+        loaderDel.parentNode.removeChild(loaderDel);
     }
-    count = 0;
-    intervalId = setInterval(function () {
-        count++;
-        if (count === 5) {
-            clearInterval(intervalId);
-            loaderDel.parentNode.removeChild(loaderDel);
-        }
-        start();
-        loader.innerHTML = '';
-        loaderIndex = -1;
-    }, 1400);
-
+    start();
+    loader.innerHTML = '';
+    loaderIndex = -1;
+}, 1400);
