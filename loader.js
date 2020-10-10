7d8395f4d7d4641ba9a7fd872948dfc5424deeab
TearingVoo <alexlisdream@gmail.com>
2020-10-10 15:17:37 +0300

    commit

--- loader.js
+++ loader.js
@@ -1,26 +1,25 @@
     let loader = document.getElementById("loader");
-    let  loaderDel= document.querySelector(".loader");
+    let loaderDel= document.querySelector(".loader");
     let loaderText = 'Loading...';
     let loaderIndex = -1;
     let timer;
     function start() {
         timer = window.setInterval(function () {
-            if (++loaderIndex == loaderText.length)
+            if (++loaderIndex === loaderText.length)
                 clearInterval(timer);
             else
                 loader.innerHTML += loaderText[loaderIndex] + '';
-        }, 200);
+        }, 100);
     }
     count = 0;
     intervalId = setInterval(function () {
         count++;
-        if (count == 5) {
+        if (count === 5) {
             clearInterval(intervalId);
             loaderDel.parentNode.removeChild(loaderDel);
         }
         start();
         loader.innerHTML = '';
         loaderIndex = -1;
-    }, 2400);
-
+    }, 1400);
 
