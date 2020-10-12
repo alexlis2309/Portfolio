ab6ad4e08684cd159a16740cc39fef09d9e354dc
TearingVoo <alexlisdream@gmail.com>
2020-10-12 17:47:06 +0300

    commit

--- loader.js
+++ loader.js
@@ -4,11 +4,14 @@ let loaderDel = document.querySelector(".loader");
 let loaderText = "Loading...";
 let index = -1;
 let index1 = -1;
-let endOfLoading = "Loading complited Write !help to get information";
+let endOfLoading = "Loading complited! Press command you need.";
 //-----------------------------------------------------------alternate typing function
-function start(text,loa,loaderIndex) {
+function start(text,loa,loaderIndex,af) {
    let timer = window.setInterval(function () {
         if (++loaderIndex === text.length) {
+            if (af===true) {
+                let menu = document.querySelectorAll(".navigate").forEach(n => n.classList.remove("hidden"));
+            }
             clearInterval(timer);
         }
         else
@@ -18,8 +21,9 @@ function start(text,loa,loaderIndex) {
                 }
             else
                 {
-                    loa.textContent += text[loaderIndex];
+                    loa.innerHTML += text[loaderIndex];
                 }
+
     }, 100);
 }
 let count = 0;
@@ -32,10 +36,10 @@ let intervalId = setInterval(function () {
     if (++count === 3) {
         clearInterval(intervalId);
         loaderDel.removeChild(loader);
-        start(endOfLoading, loader1,index1);
+        start(endOfLoading, loader1,index1,true);
     }
     else {
-        start(loaderText, loader,index);
+        start(loaderText, loader,index,false);
         loader.textContent = "";
         index=-1;
     }
@@ -78,3 +82,74 @@ let intervalId = setInterval(function () {
 //
 //     });
 // }
+//глобальная переменная со всеми таймерами для их управления
+let timeouts = {};
+
+
+//функция с обычным таймером
+function timer(form, adjust, morework)
+{
+    //создаем скорость таймера, счетчик итераций и задаем временной штамп для начала отсчета
+    let speed = 50,
+        counter = 0,
+        start = new Date().getTime();
+
+    //пример таймера
+    function instance()
+    {
+        //если флаг morework == true,
+        //выполняем некоторые вычисления для увеличения количества работы
+        if(morework)
+        {
+            for(let x=1, i=0; i<1000000; i++) { x *= (i + 1); }
+        }
+
+        //вычисляем идеальное и реальное время работы таймера
+        //судя по всему, в оригинале, автор изменил названия переменных, чтобы идентифицировать свой код при перепосте.
+        let real = (counter * speed),
+            ideal = (new Date().getTime() - start);
+
+        //увеличиваем счетчик
+        counter++;
+
+        //отображаем вычисленные значения
+        form.ideal.value = real;
+        form.real.value = ideal;
+
+        //вычисляем и отображаем разницу
+        let diff = (ideal - real);
+        form.diff.value = diff;
+
+        //если флаг adjust == true, будем вычитать разницу перед последующим вызовом (тем самым реализовав необходимое упреждение)
+        if(adjust)
+        {
+            timeouts[form.id] = window.setTimeout(function() { instance(); }, (speed - diff));
+        }
+
+        //и, если флага нет, просто вызываем следующую итерацию
+        else
+        {
+            timeouts[form.id] = window.setTimeout(function() { instance(); }, speed);
+        }
+    };
+
+    //далее, просто отбрасываем все прочь в случае обычного таймера
+    timeouts[form.id] = window.setTimeout(function() { instance(); }, speed);
+}
+//таймер с самокорректировкой
+document.getElementById('adjusting').onsubmit = function()
+{
+    //ставим флаги
+    timer(this, true, true);
+
+    return false;
+};
+
+//ресет
+document.getElementById('adjusting').onreset = function()
+{
+    //остановка таймера
+    window.clearTimeout(timeouts[this.id]);
+
+    return false;
+};
